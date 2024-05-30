72bf6a5963fb6c5f9901dd39cb5071653137951c
Rownok Jahan <rownok.cse.20220104151@aust.edu>
2024-05-30 13:54:27 +0600

    onClick

--- react-project/src/components/Navbar/Navbar.jsx
+++ react-project/src/components/Navbar/Navbar.jsx
@@ -1,28 +1,32 @@
-import { assets } from '../../assets/assets'
-import './Navbar.css'
+import  { useState } from 'react';
+import { assets } from '../../assets/assets';
+import './Navbar.css';
 
 const Navbar = () => {
+  const [menu,setMenu] = useState("Menu");
+
   return (
-    <div className='navbar'>
-    <img src={assets.logo} alt="" className="logo" />
-    <ul className="navbar-menu">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile App</li>
-        <li>Contact Us</li>
-    </ul>
-    <div className="navbar-right">
-        <img src={assets.search_icon} alt="" />
+    <div className="navbar">
+      <img src={assets.logo} alt="Logo" className="logo" />
+      <ul className="navbar-menu">
+         <li onClick={()=>setMenu("Home")}className={menu==="Home"?"active":""}>Home</li>
+         <li onClick={()=>setMenu("Menu")}className={menu==="Menu"?"active":""}>Menu</li>
+         <li onClick={()=>setMenu("Mobile App")}className={menu==="Mobile App"?"active":""}>Mobile App</li>
+         <li onClick={()=>setMenu("Contact Us")}className={menu==="Contact Us"?"active":""}>Contact Us</li>
+        
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="Search Icon" />
         <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt="" />
-            <div className="dot"></div>
+          <img src={assets.basket_icon} alt="Basket Icon" />
+          <div className="dot"></div>
         </div>
         <button>
-            Sign In
+          Sign In
         </button>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
