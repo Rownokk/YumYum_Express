fe96e75b75c84b8663167502b21a187d25edc9a5
Rownok Jahan <rownok.cse.20220104151@aust.edu>
2024-05-30 12:49:31 +0600

    First Commit

---
+++ react-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar/Navbar"
+
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ react-project/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import { assets } from '../../assets/assets'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+    <img src={assets.logo} alt="" className="logo" />
+    <ul className="navbar-menu">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile App</li>
+        <li>Contact Us</li>
+    </ul>
+    <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+            <img src={assets.basket_icon} alt="" />
+            <div className="dot"></div>
+        </div>
+        <button>
+            Sign In
+        </button>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
