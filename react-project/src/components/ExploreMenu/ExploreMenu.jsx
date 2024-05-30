fe93f7ac1da1375f52b69e4cf46b26dafb253b22
Rownok Jahan <rownok.cse.20220104151@aust.edu>
2024-05-30 16:58:14 +0600

    ExploreMenu

--- react-project/src/components/ExploreMenu/ExploreMenu.jsx
+++ react-project/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,6 +1,12 @@
+
 import './ExploreMenu.css'
+
 import { menu_list } from '../../assets/assets'
-const ExploreMenu = () => {
+        
+
+        const ExploreMenu = ({category, setCategory}) => {
+    console.log('category:', category); // Debugging line
+    console.log('setCategory:', setCategory); // Debugging line
   return (
     <div className='explore-menu'id='explore-menu'>
         <h1>Explore Our Menu</h1>
@@ -9,13 +15,15 @@ const ExploreMenu = () => {
          <div className="explore-menu-list">
             {menu_list.map((item,index)=>{
                 return (
-                    <div key={index} className='explore-menu-list-item'>
-                    <img src={item.menu_image} alt="" />
-                    <p>{item.menu_name}</p>
+                    <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key ={index} className='explore-menu-list-item'>
+                   <img className={category===item.menu_name?"active":""}src={item.menu_image} alt="item_name" />
+                   <p>{item.menu_name}</p>
                     </div>
+                  
                 )
             })}
          </div>
+         <hr/>
     </div>
   )
 }
