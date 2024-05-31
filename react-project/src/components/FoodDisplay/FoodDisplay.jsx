9d9cc151c82f7ec9f439ba26dc638b42512eba11
jamila143 <jamila.cse.20220104143@aust.edu>
2024-05-31 19:52:52 +0600

    second commit

--- react-project/src/components/FoodDisplay/FoodDisplay.jsx
+++ react-project/src/components/FoodDisplay/FoodDisplay.jsx
@@ -12,11 +12,8 @@ const FoodDisplay = ({category}) => {
         <div className="food-display-list">
       
             {food_list.map((item,index)=>{
-              {console.log(category,item.category);}
-              if (category==="All" || category===item.category){
-                return <FoodItem key={index} id={item.id} name={item.name} description={item.description}price={item.price}image={item.image}/>
-              } 
-            
+              console.log(category,item.category)
+              return <FoodItem key={index} id={item.id} name={item.name} description={item.description}price={item.price}image={item.image}/>
             })}
         </div>
     </div>
