2151aac5206eb3eb098a011737d18ebede2fa110
jamila143 <jamila.cse.20220104143@aust.edu>
2024-05-31 20:06:55 +0600

    fourth commit

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,20 +1,25 @@
+import React from "react"
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar"
 import Home from "./pages/Home/Home"
 import Cart from "./pages/Cart/Cart"
 import PlaceOrder from "./pages/placeOrder/placeOrder"
+import Footer from "./components/Footer/Footer"
 const App = () => {
   return (
-    <div className="app">
-      <Navbar/>
-      <Routes>
-        <Route path='/'element = {<Home/>}/>
-        
-        <Route path='/cart'element ={<Cart/>}/>
-        <Route path='/placeOrder'element ={<PlaceOrder/>}/>
-        
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/placeOrder' element={<PlaceOrder />} />
+
+        </Routes>
+      </div>
+      <Footer />
+    </>
   )
 }
 
