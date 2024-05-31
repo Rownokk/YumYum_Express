76adee16eccf073f13a137f93904f1d8bd0d3933
Rownok Jahan <rownok.cse.20220104151@aust.edu>
2024-05-31 14:29:18 +0600

    FoodItem

--- react-project/src/context/storeContext.jsx
+++ react-project/src/context/storeContext.jsx
@@ -1,12 +1,13 @@
 
 import { createContext } from 'react';
 import PropTypes from 'prop-types';
-
+import { food_list } from '../assets/assets';
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     const contextValue = {
         // add your context value here
+        food_list
     };
 
     console.log("StoreContextProvider rendered"); // Debugging output
--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -1,16 +1,12 @@
-
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {BrowserRouter} from 'react-router-dom'
-import StoreContextProvider from './context/StoreContext.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import StoreContextProvider from "./context/StoreContext.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <StoreContextProvider>
-  <App />
-  </StoreContextProvider>
-  
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
   </BrowserRouter>
-    
-  
-)
+);
