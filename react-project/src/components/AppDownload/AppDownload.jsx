b3c8eb0440afc26dffd5163cd25a3e1be607af19
Rownok Jahan <rownok.cse.20220104151@aust.edu>
2024-06-01 00:35:11 +0600

    color+font

--- react-project/src/components/AppDownload/AppDownload.jsx
+++ react-project/src/components/AppDownload/AppDownload.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+
 import './AppDownload.css'
 import { assets } from '../../assets/assets'
 
 const AppDownload=() =>{
     return(
         <div className='app-download' id='app-download'>
-            <p>For Betteer Experience Download <br /> Tomato App</p>
+            <p>For Betteer Experience Download <br />YumYum_Express Mobile App</p>
             <div className="app-download-platforms">
             <img src={assets.play_store} alt="" />
             <img src={assets.app_store} alt="" />
