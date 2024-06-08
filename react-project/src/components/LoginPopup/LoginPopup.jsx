11f36cf4e740d9cdc27006fcdd21be6eac26f738
Rownok Jahan <rownok.cse.20220104151@aust.edu>
2024-06-08 14:06:45 +0600

    signup

--- react-project/src/components/LoginPopup/LoginPopup.jsx
+++ react-project/src/components/LoginPopup/LoginPopup.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginPopup.css'
 const LoginPopup = () => {
+
+    const [currState,setCurrState]=useState("Sign Up")
   return (
     <div className='login-popup'>
-        
+        <form className='login-popup-container'>
+           <div className="login-popup-title">
+            <h2>{currState}</h2>
+            </div> 
+        </form>
     </div>
   )
 }
