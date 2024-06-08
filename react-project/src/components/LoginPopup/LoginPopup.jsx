53205eb7330708cefd5541ef7350e70bab9b5199
Rownok Jahan <rownok.cse.20220104151@aust.edu>
2024-06-08 14:16:36 +0600

    placeholder

--- react-project/src/components/LoginPopup/LoginPopup.jsx
+++ react-project/src/components/LoginPopup/LoginPopup.jsx
@@ -11,6 +11,14 @@ const LoginPopup = ({setShowLogin}) => {
             <h2>{currState}</h2>
             <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
             </div> 
+            <div className="login-popup-inputs">
+              <input type='text'placeholder='Your name'  required/>
+              <input type='email'placeholder='Your email'  required/>
+              <input type='password'placeholder='Password'  required/> 
+            </div>
+            <button>
+                {currState==="Sign Up"?"Create account":"Login"}
+            </button>
         </form>
     </div>
   )
