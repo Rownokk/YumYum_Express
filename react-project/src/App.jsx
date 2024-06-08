ea99e324580818b44f3bfc18961ddd95029e2d96
Rownok Jahan <rownok.cse.20220104151@aust.edu>
2024-06-08 14:10:58 +0600

    crossIcon

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   const [showLogin,setShowLogin] = useState(false)
   return (
     <>
-    {showLogin?<LoginPopup/>:<></>}
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
       <div className="app">
         <Navbar setShowLogin={setShowLogin}/>
         <Routes>
--- react-project/src/components/LoginPopup/LoginPopup.jsx
+++ react-project/src/components/LoginPopup/LoginPopup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './LoginPopup.css'
-const LoginPopup = () => {
+import { assets } from '../../assets/assets'
+const LoginPopup = ({setShowLogin}) => {
 
     const [currState,setCurrState]=useState("Sign Up")
   return (
@@ -8,6 +9,7 @@ const LoginPopup = () => {
         <form className='login-popup-container'>
            <div className="login-popup-title">
             <h2>{currState}</h2>
+            <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
             </div> 
         </form>
     </div>
