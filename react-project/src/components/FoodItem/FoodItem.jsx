7a36492334f317a1748c86ed6262134d6ff7f82e
jamila143 <jamila.cse.20220104143@aust.edu>
2024-06-13 13:53:20 +0600

    twentith commit

--- react-project/src/components/FoodItem/FoodItem.jsx
+++ react-project/src/components/FoodItem/FoodItem.jsx
@@ -1,33 +1,43 @@
-// FoodItem.js
-
-import React, { useContext } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { assets } from '../../assets/assets';
 import './FoodItem.css';
 import { StoreContext } from '../../context/StoreContext';
-import { assets } from '../../assets/assets';
 
 const FoodItem = ({ id, name, price, description, image }) => {
-  const { addToCart } = useContext(StoreContext);
-  const history = useHistory();
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+  const [count, setCount] = useState(0); // State to manage the count for the current item
 
   const handleAddToCart = () => {
-    addToCart(id); // Add item to cart
-    history.push('/cart'); // Navigate to cart page immediately after adding to cart
+    setCount(count + 1); // Increment the count when adding to cart
+    addToCart(id);
+  };
+
+  const handleRemoveFromCart = () => {
+    if (count > 0) {
+      setCount(count - 1); // Decrement the count when removing from cart
+      removeFromCart(id);
+    }
   };
 
   return (
     <div className='food-item'>
       <div className="food-item-img-container">
         <img className='food-item-image' src={image} alt="" />
+        <div className='food-item-counter'>
+          {count > 0 && (
+            <img onClick={handleRemoveFromCart} src={assets.remove_icon_red} alt="" />
+          )}
+          {count > 0 && <p>{count}</p>}
+          <img onClick={handleAddToCart} src={assets.add_icon_green} alt="" />
+        </div>
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
           <p>{name}</p>
-          <img src={assets.rating_starts} alt="Rating" />
+          <img src={assets.rating_starts} alt="" />
         </div>
         <p className='food-item-desc'>{description}</p>
         <p className='food-item-price'>Tk.{price}</p>
-        <button className="add-to-cart-button" onClick={handleAddToCart}>Add to Cart</button>
       </div>
     </div>
   );
