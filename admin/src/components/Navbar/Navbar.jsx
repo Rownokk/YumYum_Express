ad1707ec329d80b6619bce7c71a040ee2e4699ba
jamila143 <jamila.cse.20220104143@aust.edu>
2024-06-20 20:11:18 +0600

    27 commit

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import './Navbar.css'
-import {assets} from '../../assets/assets'
-const Navbar=()=>{
-return(
-    <div className='navbar'>
-        <img className='logo' src={assets.logo} alt="" />
-        <img className='profile' src={assets.profile_image} alt="" />
+import React from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';  // Import Link from react-router-dom
+import { assets } from '../../assets/assets';
 
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <Link to='/' className="logo">
+       
+        <div className="logo-text">
+          <span className="logo-line1">YumYum</span>
+          <span className="logo-line2">Express</span>
+        </div>
+      </Link>
+      <img className='profile' src={assets.profile_image} alt="Profile" />
     </div>
-    )
-}
-export default Navbar
+  );
+};
+
+export default Navbar;
