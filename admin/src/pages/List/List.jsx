86e62dddcde269c44c195a8393678edf213fe3fa
Authoi Chowdhury <authoichy016@gmail.com>
2024-06-26 22:08:21 +0600

    list

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -3,15 +3,14 @@ import './List.css';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const List = () => {
-    const url = 'http://localhost:2000';
+const List = ({url}) => {
+    
     const [list, setList] = useState([]);
 
     const fetchList = async () => {
         try {
             const response = await axios.get(`${url}/api/food/list`);
-
-            console.log(response.data);
+        
             if (response.data.success) {
                 setList(response.data.data);
             } else {
@@ -22,6 +21,21 @@ const List = () => {
             toast.error('Error fetching data');
         }
     };
+
+    const removeFood = async (foodId) => {
+        try {
+            const response = await axios.post(`${url}/api/food/remove`, { id: foodId });
+            if (response.data.success) {
+                toast.success('Food item removed successfully');
+                fetchList();
+            } else {
+                toast.error('Error removing food item');
+            }
+        } catch (error) {
+            console.error('Error removing food item:', error);
+            toast.error('Error removing food item');
+        }
+    }
 
     useEffect(() => {
         fetchList();
@@ -40,12 +54,11 @@ const List = () => {
                 </div>  
                 {list.map((item, index) => (
                     <div key={index} className='list-table-format'>
-                        {/* Ensure correct image path */}
                         <img src={`${url}/images/${item.image}`} alt={item.name} />
                         <p>{item.name}</p>
                         <p>{item.category}</p>
                         <p>Tk.{item.price}</p>
-                        <p>X</p>
+                        <p onClick={() => removeFood(item._id)} className='cursor remove-action'>X</p>
                     </div>
                 ))}
             </div>
