a8c7081118c24795e158a18807b09a2421a1f985
Rownok Jahan <rownok.cse.20220104151@aust.edu>
2024-07-04 23:19:36 +0600

    usercontroller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,17 @@
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import validator from "validator"
+
+
+//login user
+const loginUser = async (req,res)=> {
+
+}
+
+//register user
+const registerUser = async (req,res)=>{
+
+}
+
+export {loginUser,registerUser}
