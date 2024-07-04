c0dad3694d722255342e295500816c8bd288eb3f
Rownok Jahan <rownok.cse.20220104151@aust.edu>
2024-07-04 23:28:13 +0600

    server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
 
 
 
@@ -19,6 +20,8 @@ connectDB();
 //api endpoints
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+
 
 app.get("/",(req,res)=>{
     res.send("API Working")
