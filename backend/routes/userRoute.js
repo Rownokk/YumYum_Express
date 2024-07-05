3df661aa3452470e32921846f952a7ee7b6866f2
Rownok Jahan <rownok.cse.20220104151@aust.edu>
2024-07-05 13:54:48 +0600

    register&login Api success

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,6 +4,6 @@ import { loginUser,registerUser } from "../controllers/userController.js"
 const userRouter =express.Router()
 
 userRouter.post("/register",registerUser)
-userRouter.post("login",loginUser)
+userRouter.post("/login",loginUser)
 
 export default userRouter;
