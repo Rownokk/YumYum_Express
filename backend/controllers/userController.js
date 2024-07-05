b37393ea8fb4741bab83bd1945f3ab32defb2f97
Rownok Jahan <rownok.cse.20220104151@aust.edu>
2024-07-05 13:36:46 +0600

    userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,9 +9,47 @@ const loginUser = async (req,res)=> {
 
 }
 
+const createToken = (id)=>{
+    return jwt.sign({id},process.env.JWT_SECRET)
+}
 //register user
 const registerUser = async (req,res)=>{
+    const {name,password,email} =req.body;
+    try {
+    //checking is user already exists
+        const exists = await userModel.findOne({email});
+        if(exists){
+            return res.json({success:false,message:"User Alredy Exists"})
+        }
 
+        //validating email format & strong password
+        if(!validator.isEmail(email)){
+            return res.json({success:false,message:"Please enter a valid email"})
+        }
+
+        if(password.length<8){
+            return res.json({success:false,message:"please enter a strong password"})
+        }
+
+        //hashing user password
+        const salt =await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        const newUser =new userModel({
+            name:name,
+            email:email,
+            password:hashedPassword
+        })
+
+      const user =  await newUser.save()
+      const token =createToken(user._id)
+      res.json({success:true,token});
+
+
+    } catch (error) {
+       console.log(error);
+       res.json({success:false,message:"Error"}) 
+    }
 }
 
 export {loginUser,registerUser}
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
-
+import 'dotenv/config'
 
 
 //app config
