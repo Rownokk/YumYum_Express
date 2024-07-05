de929838612d336f6967e22a1f30024452de5fb5
Rownok Jahan <rownok.cse.20220104151@aust.edu>
2024-07-05 14:37:11 +0600

    frontend connection with backend

--- react-project/src/components/LoginPopup/LoginPopup.jsx
+++ react-project/src/components/LoginPopup/LoginPopup.jsx
@@ -4,6 +4,18 @@ import { assets } from '../../assets/assets'
 const LoginPopup = ({setShowLogin}) => {
 
     const [currState,setCurrState]=useState("Login")
+   const [data,setData]=useState({
+    name:"",
+    email:"",
+    password:""
+   }) 
+
+  const onChangeHandler = (event)=>{
+    const name= event.target.name;
+    const value=event.target.value;
+   setData(data=>({...data,[name]:value})) 
+
+  }
   return (
     <div className='login-popup'>
         <form className='login-popup-container'>
@@ -12,10 +24,10 @@ const LoginPopup = ({setShowLogin}) => {
             <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
             </div> 
             <div className="login-popup-inputs">
-                {currState==="Login"?<></>:<input type='text'placeholder='Your name'  required/>}
+                {currState==="Login"?<></>:<input name='name'onChange={onChangeHandler} value={data.name}  type='text'placeholder='Your name'  required/>}
               
-              <input type='email'placeholder='Your email'  required/>
-              <input type='password'placeholder='Password'  required/> 
+              <input name='email'onChange={onChangeHandler} value={data.email} type='email'placeholder='Your email'  required/>
+              <input name='password'onChange={onChangeHandler} value={data.password} type='password'placeholder='Password'  required/> 
             </div>
             <button>
                 {currState==="Sign Up"?"Create account":"Login"}
