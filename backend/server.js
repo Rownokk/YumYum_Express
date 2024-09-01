5685c9633c2eb2b5b39aabfa6e95de89692905f1
Rownokk <mowmita878@gmail.com>
2024-09-01 19:36:02 +0600

    port

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import orderRouter from "./routes/orderRoute.js"
 
 //app config
 const app = express()
-const port= 2000
+const port= process.env.PORT || 2000;
 
 // middleware
 app.use(express.json())
